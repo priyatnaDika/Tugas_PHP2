6e7e65a886e761c0b17963d0798fff44f32625f9
priyatnaDika <ngotaxdong008@gmail.com>
2021-03-07 16:07:40 +0800

    project saya

---
+++ index.php
@@ -0,0 +1,149 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>ul { list-style: none; margin: 0; padding: 0; overflow: hidden; background-color: orangered;} li{ float: left;} li a{display: block;color: white;text-align: center; padding: 14px; text-decoration: none;} </style>
+    <title>Latihan 1</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="robots" content="index, folow, max-snippet:-1, max-imagepreview:large, max-video-preview:-1">
+    <link rel="canonical" href="http://informatika.undiksha.ac.id/">
+    <meta property="og:locale" content="en_US">
+    <meta property="og:type" content="website">
+    <meta property="og:title" content="??">
+    <meta property="og:description" content="??">
+    <meta property="og:url" content="??">
+    <meta property="og:site_name" content="??">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/
+    css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js
+    "></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min
+    .js"></script>
+</head>
+
+<body>
+        
+        <ul class="menusidebar">
+            <li class="activate"><a href="http://localhost/Latihan2_PHP/#">Home</a></li>
+            <li><a href="">Artikel</a></li>
+            <li><a href="">ESPORTS</a></li>
+            <li><a href="">Forum</a></li>
+            <li><a href="http://localhost/Latihan2_PHP/login.php">LOGIN</a></li>
+        </ul>
+    
+    
+    <div class="post-info" style="float: left;">
+        Upload by <b>ADMIN</b>
+    </div>
+
+     <div class="jumbotron text-center", style=" margin: 100px; color: tomato; height: 90px;
+    " >
+        <center>
+        <h1> <b> GameBoy  <p>Eat|Sleep|Game|Repeat</p>
+        <img src="media/projekAkhir.gif", ></b> </h1>
+        </center>
+      
+    </div>
+
+    <p></p>
+    <p></p>
+    <p></p>
+
+     <div class="container" style=" color: white; ;
+    " >">
+        <div class="row" style="margin: 200px; padding: 400px;">
+            <div >
+                <h3 >GAME SYSTEM <br> <br></h3>
+                <img src="media/IBLdfSU.gif", class="rounded float-left", style="margin: 100;">
+                <p></p>
+
+    <p >
+      <br>  Valorant adalah 5 versus 5 characterbased tactical shooter. Setiap pertandingan akan berisi 25 round dan tim pertama
+    yang berhasil meraih 13 round kemenangan akan memenangkan pertandingan. Anda dapa
+    t memilih Agent di awal pertandingan. Berbeda dengan Overwatch, Anda tidak bisa m
+    engganti Agent di tengah pertandingan. Mode gamenya identik dengan CS:GO yaitu berupa bomb mission. Karena game FPS membutuhkan t
+    ingkat presisi yang tinggi, Riot Games akan menyediakan 128 tickservers untuk Valorant.
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    </p>
+
+    <p></p>
+    <p></p>
+    <p></p>
+    <p></p>
+    </div> 
+    
+    <div >
+        <h3>Karakter dengan skill hasil gabungan dari Overwatch, Apex Leg
+        ends dan Rainbow Six Siege <br> <br> </h3>
+        <img src="media/13-2.gif", class="imgrounded" width="800px", height="400px">
+        <p></p>
+
+    <p> <br> Dikutip dari ESTNN, ada delapan karakter yang diperkenalkan pe
+    rtama kali di Valorant. Setiap Agent memiliki skill yang berbeda satu sama lain.
+    Skill setiap karakter dikategorikan menjadi tiga yaitu purchasable abilities, ult
+    imate, dan main ability. Purchasable abilities adalah skill yang bisa dibeli saat
+    memulai ronde. Anda dapat membeli dua jenis skill yang tersedia. Ultimate skill
+    didapat dari membunuh musuh dan menyelesaikan objektif. Mekanik ultimate yang sam
+    a dengan Overwatch. Main ability adalah skill yang tersedia di setiap karakter se
+    cara cuma-cuma. Main ability membutuhkan cooldown pada setiap penggunaannya.
+    <br>
+    <br>
+
+    </p>
+
+    <p>Tidak seperti Overwatch yang berfokus pada skill, di Valorant
+    ini kamu masih tetap mengutamakan penggunaan senjata untuk berperang. Bisa dibila
+    ng bahkan skill dari tiap karakter yang ada bukanlah penentu kemenangan karena ti
+    ap skill memiliki kegunaannya masingmasing, serta skill tersebut masih bisa diatasi dengan mudah.</p>
+    
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>    
+
+    </div>
+
+    <div >
+
+         <h3>System Requirements <br> <br> </h3>
+
+        <img src="media/tenor.gif", class="rounded float-left">
+         <p></p>
+
+    <p> <br> Buat kalian yang memiliki laptop dengan spesifikasi yang terbi
+    lang paspasan tapi ingin bermain Valorant sebaiknya kalian tidak usah khawatir karena Val
+    orant membutuhkan spesifikasi yang tidak begitu tinggi untuk dijalankan dengan la
+    ncar. Jika kamu ingin memainkan Valorant dengan frame per second ratarata sebesar 30 FPS, maka setidaknya laptop atau PC kamu menggunakan prosesor Int
+    el i3-
+    370M dengan VGA Intel HD 4000. Dengan spesifikasi tersebut kamu bisa memainkan Va
+    lorant di 30 FPS dengan pengaturan grafis rata kiri. Sedangkan untuk menjalankan
+    di 60 FPS, kamu hanya perlu menggunakan prosesor Intel i3-
+    4150 dengan VGA Geforce GT 730.</p> 
+    
+    <p>
+    <br>  Kesimpulannya, Valorant ini sangatlah cocok untuk dimainkan ol
+    eh pecinta game FPS, khususnya FPS berjenis tactical shooter. Jika kamu mulai mer
+    asa bosan dengan game FPS yang sedang kamu mainkan, maka tidak ada salahnya kamu
+    mencoba Valorant untuk merasakan gameplay yang baru dan fresh!
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    </p>
+        
+            </div>
+         </div>
+    </div>
+    
+</body>
---
+++ register.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+         <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initialscale=1.0">
+        <title>BotGame Sign-Up</title>
+        <link rel="stylesheet" href="register_style.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <div class="container">
+ 
+            <div class="title">Sign-Up</div>
+            <form action="#">
+            <div class="user">
+            <div class="input">
+            <span class="detail">Nama Depan</span>
+            <input type="text" placeholder="Masukkan Nama Depan Anda"> </div>
+            <div class="input">
+            <span class="detail">Nama Belakang</span>
+            <input type="text" placeholder="Masukkan Nama Belakang Anda">
+            </div>
+
+
+            <div class="input">
+                <span class="detail">Username</span>
+                <input type="text" placeholder="Masukkan Username Anda">
+            </div>
+
+            <div class="input">
+               <span class="detail">Email</span>
+                <input type="text" placeholder="Masukkan Email Anda">
+            </div>
+
+           <div class="input">
+             <span class="detail">Password</span>
+               <input type="text" placeholder="Masukkan Password Anda">
+           </div>
+
+           <div class="input">
+               <span class="detail">Konfirmasi Password</span>
+               <input type="text" placeholder="konfirmasi password Anda">
+           </div>
+
+        </div>
+
+        <div class="button">
+             <button type="submit" name="sign-Up" class="btn-login">Sign-Up</button>
+        </div>
+
+ </form>
+ </div>
+ </body>
+</html>
